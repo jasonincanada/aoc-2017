2267f1129271041c7aaef6ef31f3289d8c38eaea
nirgle <=>
2018-02-05 14:04:53 -0500

    Day 3

---
+++ 3-1.hs
@@ -0,0 +1,33 @@
+{- Advent of Code 2017
+
+   Day: 3 - Spiral Memory
+   URL: http://adventofcode.com/2017/day/3
+
+   Part 1
+-}
+
+-- Highest number on ring i
+ringMax :: Int -> Int
+ringMax i = (2*i+1)^2
+
+-- Index of the ring that n is found on
+findRing :: Int -> Int
+findRing n = let tuples = map (\x -> (x, ringMax x)) [0..]
+                 list = filter (\(i, max) -> max >= n) tuples
+             in  fst $ head list
+
+-- Compute number of steps to the middle. This is the index of the ring
+-- plus the distance from the target number to the nearest cardinal
+steps :: Int -> Int
+steps n = let i = findRing n
+              max = ringMax i
+              size = ringMax i - ringMax (i-1)
+              halfrow = size `div` 4 `div` 2
+              cardinals = map (\x -> max - ((size*x) `div` 4) + halfrow) [1..4]
+              distances = [ abs (n-c) | c <- cardinals ]
+              closest = minimum distances
+          in  i + closest
+
+main = do
+  print $ steps 368078
+
