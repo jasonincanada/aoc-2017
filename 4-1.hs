4eda2b8768ba5542bfafafb8d732a54b8964228c
nirgle <=>
2018-02-10 11:55:21 -0500

    Day 4

---
+++ 4-1.hs
@@ -0,0 +1,23 @@
+{- Advent of Code 2017
+
+   Day: 4 - High-Entropy Passphrases
+   URL: http://adventofcode.com/2017/day/4
+
+   Part 1
+-}
+
+import Data.List
+
+unique :: String -> [String]
+unique = map head . group . sort . words 
+
+isValid :: String -> Bool
+isValid s = let all = words s
+                uniq = unique s
+            in  length all == length uniq
+
+main = do
+  file <- readFile "4.input"
+  let ls = lines file
+  print $ length $ filter isValid ls
+
