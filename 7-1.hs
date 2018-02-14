b906d829d74c2124b2204e899e0041bc6e171557
nirgle <=>
2018-02-14 18:09:57 -0500

    Day 7, part 1

---
+++ 7-1.hs
@@ -0,0 +1,45 @@
+{- Advent of Code 2017
+
+   Day: 7 - Recursive Circus
+   URL: http://adventofcode.com/2017/day/7
+
+   Part 1
+-}
+
+data Program = Program { name :: String,
+                         weight :: String,
+                         children :: [String] } deriving (Show)
+
+-- Parse a line our internal Program type
+parse :: String -> Program
+parse s = let ws = words s
+              l = length ws
+              name = ws !! 0
+              weight = ws !! 1
+              children = map (filter (/=',')) $ drop 3 ws
+          in  Program { name = name, 
+                        weight = weight, 
+                        children = children }
+
+-- Given a program, find its parent in the passed list of all programs
+parent :: Program -> [Program] -> Maybe Program
+parent p [] = Nothing
+parent p (c:cs)
+  | name p `elem` children c = Just c
+  | otherwise                = parent p cs
+
+process :: [Program] -> String
+process [] = "No input"
+process (p:ps) = go p ps
+  where
+    go :: Program -> [Program] -> String
+    go p ps = case parent p ps of
+                Nothing -> name p
+                Just p  -> go p ps
+
+main = do
+  file <- readFile "7.input"
+  let input = lines file
+  let programs = map parse input
+  print $ process $ programs
+  
