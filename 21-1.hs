ecd8cfc990b265dd980328fd5b77847f8d165acf
nirgle <=>
2018-03-05 14:06:34 -0500

    Day 21, part 1

---
+++ 21-1.hs
@@ -0,0 +1,80 @@
+{- Advent of Code 2017
+
+   Day: 21 - Fractal Art
+   URL: http://adventofcode.com/2017/day/21
+
+   Part 1
+-}
+
+import Data.List (transpose, nub, lookup) 
+import Data.List.Split (splitOn, chunksOf)
+
+type Pattern = [String]
+type Grid = [[Pattern]]
+type Enhancement = (Pattern, Pattern)
+
+-- 012    630    01    30
+-- 345 -> 741    23 -> 21
+-- 678    852
+mirror, rotate :: Pattern -> Pattern
+mirror = map reverse
+rotate = map reverse . transpose
+
+-- Build the list of distinct symmetries for this pattern
+rotations :: Pattern -> [Pattern]
+rotations p = nub (rotates ++ flips)
+  where rotates = take 4 $ iterate rotate p
+        flips   = take 4 $ iterate rotate (mirror p)
+
+parse :: String -> Enhancement
+parse s = (splitOn "/" left, splitOn "/" right)
+  where left  = ws !! 0
+        right = ws !! 2
+        ws    = words s
+
+-- Join a grid of patterns into one pattern
+join :: Grid -> Pattern
+join = concat . map (foldr f empty)
+  where f     = zipWith (++)
+        empty = cycle [[]]
+
+-- Split a pattern into a grid of n-by-n patterns
+subdivide :: Int -> Pattern -> Grid
+subdivide n p = let rows = chunksOf n p
+                in  map (transpose . map (chunksOf n)) rows
+            
+count :: Pattern -> Int
+count = sum . map (length . filter (=='#'))
+
+-- Get the side length of a pattern
+size :: Pattern -> Int
+size n = length (n !! 0)
+
+process :: [Enhancement] -> Pattern -> Pattern
+process es pat = let n    = size pat
+                     div  = if n `mod` 2 == 0
+                            then 2
+                            else 3
+                     grid = subdivide div pat
+                 in  join $ map (map replace) grid
+  where 
+    -- Look up a pattern by its various symmetries
+    replace :: Pattern -> Pattern
+    replace p = let rs    = rotations p
+                    tries = [ lookup try es | try <- rs ]
+                in  firstJust tries
+
+    firstJust :: [Maybe Pattern] -> Pattern
+    firstJust []     = error "No replacement pattern found"
+    firstJust (p:ps) = case p of
+                         Just pat -> pat
+                         Nothing -> firstJust ps
+
+start :: Pattern
+start = [".#.", "..#", "###"]
+
+main = do
+  file <- readFile "21.input"
+  let enhancements = map parse $ lines file
+  print $ count $ last $ take 6 $ iterate (process enhancements) start
+
